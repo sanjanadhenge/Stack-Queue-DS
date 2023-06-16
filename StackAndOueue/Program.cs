8d73aa6b0de452c6d8918f3adb5660f55820c049
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-06-16 20:37:46 +0530

    [sanjana] Add : Enqueu Operations

--- StackAndOueue/Program.cs
+++ StackAndOueue/Program.cs
@@ -11,16 +11,21 @@ namespace StackAndQueue
     {
         static void Main(string[] args)
         {
-            StackDS stack = new StackDS();
-             stack.Push(70);
-             stack.Push(30);
-             stack.Push(56);
-             stack.Display();
-             stack.Peek();
-             stack.Pop();
-             stack.Pop();
-             stack.Pop();
-             stack.Display();
+            //StackDS stack = new StackDS();
+            // stack.Push(70);
+            // stack.Push(30);
+            // stack.Push(56);
+            // stack.Display();
+            // stack.Peek();
+            // stack.Pop();
+            // stack.Pop();
+            // stack.Pop();
+            // stack.Display();
+            QueueDs queue = new QueueDs();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
 
         }
     }
